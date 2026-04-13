2a05bbc1bd325daaeefdd3c22c4ea99772ad69dc
nareshyadav20 <nareshnareshyadav20@gmail.com>
2026-04-13 21:14:12 +0530

    changes mades

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const axios = require("axios");
 const { spawn } = require("child_process");
